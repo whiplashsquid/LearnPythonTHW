ca8172ac9405ed54684e4ed051cef4c3e79d4125
Anna Richardson <whiplashsquid@gmail.com>
2017-10-14 13:02:50 +0100

    writing my own read and write script

--- ex16edit.py
+++ ex16edit.py
@@ -10,14 +10,10 @@ print("If you do want that, hit RETURN.")
 input("?")
 # prints string
 print("Opening the file....")
-# opens file bound to filename from command line in write mode and assigns contents to variable name target
+# opens file bound to filename from command line in write mode (otherwise defaults to 'r' read for safety) and assigns contents to variable name target
 target = open(filename, 'w')
-# prints string
-print("Truncating the file. Goodbye!")
-# applies truncate function with no variable to target variable (ie. your file) thereby erasing all previous contents
-target.truncate()
 # prints strings
-print("Now I'm going to ask you for three lines.")
+print("I'm going to ask you for three lines.")
 # prompts for input with string, binds each input in turn to variable names line1, line2, line3
 line1 = input("line 1: ")
 line2 = input("line 2: ")
@@ -25,13 +21,10 @@ line3 = input("line 3: ")
 # prints string
 print("I'm going to write these to file.")
 # applies write function with variable given in () to file associated with target variable name. Nb. each \n writes to newline
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(f"{line1}\n{line2}\n{line3}\n")
 # prints string
-print("And finally, we close it.")
+print(f"""
+And finally, we close it.
+Try using ex16read.py to check the new contents of {filename}""")
 # closes file associated with variable name target
 target.close()
