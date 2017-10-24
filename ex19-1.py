8cce3d76f0c4b0fa0104d62ae423ae923ce37818
Anna Richardson <whiplashsquid@gmail.com>
2017-10-24 12:12:19 +0100

    printing line by line

---
+++ ex19-1.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+script, filename = argv
+
+# define my function to open, read, write to and close a file using input prompted for from the user
+def amend_file(filename):
+    print(f")
