a65262c7de5b5bed19b0f9def26106a30e7dc7e4
Anna Richardson <whiplashsquid@gmail.com>
2017-10-31 12:29:52 +0000

    building lists, using .append or range(); print lists using for-loop

--- ex32a.py
+++ ex32a.py
@@ -1,4 +1,4 @@
-the_count = [1, 2, 3, 4, 5]
+the_count = [range(1,6)]
 fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
@@ -15,14 +15,8 @@ for fruit in fruits:
 for i in change:
     print(f"I got {i}")
 
-# we can also build lists, first start with an empty one
-elements = []
-
-# then use the range function to do 0 to 5 counts
-for i in range(0,6):
-    print(f"Adding {i} to the list.")
-    # append is a function that lists understand
-    elements.append(i)
+# we can create a numerical list with a range
+elements = [range(6)]
 
 # now we can print them out too
 for i in elements:
