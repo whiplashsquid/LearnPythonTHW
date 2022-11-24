44411e95f32c7a2d16855d67d7adda8f2d0fafc1
Anna <whiplashsquid@gmail.com>
2022-11-24 14:34:26 +0000

    Delete ex19-1.py

--- ex19-1.py
+++
@@ -1,7 +0,0 @@
-from sys import argv
-
-script, filename = argv
-
-# define my function to open, read, write to and close a file using input prompted for from the user
-def amend_file(filename):
-    print(f")
