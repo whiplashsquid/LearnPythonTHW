23766f0420bf1c5afa3336eb13000af55a00ffb3
Anna <whiplashsquid@gmail.com>
2022-11-24 14:25:19 +0000

    Delete ex11-practice.py

--- ex11-practice.py
+++
@@ -1,7 +0,0 @@
-print("How old are you?", end=' ')
-age = int(input())
-x2 = age * 2
-plusten = x2 + 10
-name = input("What's your name? ")
-print(f"Hey there, {name}!")
-print(f"So when you are twice as old as you are now, you'll be {x2} years old and ten years after that you'll be {plusten} years old.")
